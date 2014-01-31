ff8aca65e5607e84b8c5848c2167444d86965cf6
Mosky <mosky.tw@gmail.com>
2014-01-31 22:31:38 +0800

    simply defined the names of each columns

--- zipcodetw.py
+++ zipcodetw.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import csv
+
+with open('zipcodetw-20140131.csv') as f:
+    for row in csv.reader(f):
+        zipcode = row[0]
+        address_tuple = row[1:-1]
+        rule_str = row[-1]
+        print zipcode, address_tuple, rule_str
