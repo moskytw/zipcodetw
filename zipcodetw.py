97648934a350cd8e00e5819c268168ed7c52b77f
Mosky <mosky.tw@gmail.com>
2014-01-31 22:39:53 +0800

    should skip the title

--- zipcodetw.py
+++ zipcodetw.py
@@ -4,8 +4,13 @@
 import csv
 
 with open('zipcodetw-20140131.csv') as f:
+
+    next(f)
+
     for row in csv.reader(f):
+
         zipcode = row[0]
         address_tuple = row[1:-1]
         rule_str = row[-1]
+
         print zipcode, address_tuple, rule_str
