5deeeb3993925850d3deeaa87aad8b6f524c18fd
Toomore Chiang <toomore0929@gmail.com>
2014-01-31 21:31:31 +0800

    Add full address for test.

--- tobolist.py
+++ tobolist.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import csv
 from difflib import get_close_matches
+from random import choice
 
 with open('./Zip32_10301.csv') as files:
     csv_files = csv.reader(files)
     csv_files = [' '.join(i) for i in csv_files]
-    #print csv_files[5]
+    print u'測試：', choice(csv_files)
 
-address = u'高雄市三民區大昌二路'
-print address
+address = u'高雄市三民區大昌二路307-1號'
+print u'查詢：', address
+print u'結果：'
 for i in get_close_matches(address.encode('utf-8'), csv_files):
     print i
