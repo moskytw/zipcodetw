23f70fb3f97f411a41021cd8d2a7177daed3f7f2
Mosky <mosky.tw@gmail.com>
2014-02-01 02:35:33 +0800

    fixup! e2b7c2d

--- zipcodetw.py
+++ zipcodetw.py
@@ -9,8 +9,6 @@ addr_token_re = re.compile(u'''
     (?P<unit>[縣市鄉鎮市區村里路街巷弄號樓])
 ''', re.X)
 
-addr_str = u'臺北市信義區市府路1之23號'
-
 rule_token_re = re.compile(u'''
     (?P<prefix>[全單雙連至])?
     (?P<number>.+?)
@@ -61,5 +59,6 @@ print fail_count
 
 # test addr_token_re
 
+addr_str = u'臺北市信義區市府路1之23號'
 print addr_str
 print addr_token_re.findall(addr_str)
