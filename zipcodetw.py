6f1ecd9bc0ec69864c5b04a5054f4d688a23f69a
Mosky <mosky.tw@gmail.com>
2014-02-02 20:08:46 +0800

    added AddressRule class

--- zipcodetw.py
+++ zipcodetw.py
@@ -80,31 +80,100 @@ print addr3 < addr4
 print addr4 < addr5
 #print addr5 < addr6 # ValueError
 
-rule_token_re = re.compile(u'''
-    (?P<special>[單雙至]|以下|以上)
-    |
-    (?P<number>\d+)
-    (?:[之-](?P<sub_number>\d+))?
-    (?P<unit>[巷弄號樓]|附號)
-''', re.X)
+class AddressRule(Address):
 
-#test_cases = [
-#    u'全', u'單全',
-#    u'雙48號以下', u'雙50號以上',
-#    u'7號',
-#    u'連2之4號以上',
-#    u'14號含附號', u'14巷全',
-#    u'47號', u'47附號全',
-#    u'雙68巷至70號含附號全',
-#    u'連49號含附號以下',
-#    u'1之3號及以上附號',
-#]
-#
-#for test_case in test_cases:
-#    print test_case
-#    print rule_token_re.findall(test_case)
-#
-#import sys; sys.exit()
+    RULE_TOKEN_RE = re.compile(u'''
+        [連全單雙至]
+        |
+        以下|以上
+        |
+        含附號|及以上附號|含附號以下
+        |
+        附號全
+    ''', re.X)
+
+    @staticmethod
+    def extract_tokens(addr_rule_str):
+
+        if isinstance(addr_rule_str, str):
+            addr_rule_str = addr_rule_str.decode('utf-8')
+
+        rule_tokens = []
+
+        def extract_token(m):
+            token = m.group()
+            rule_tokens.append(token)
+            if token == u'附號全':
+                return u'號'
+            return ''
+
+        addr_str = AddressRule.RULE_TOKEN_RE.sub(extract_token, addr_rule_str)
+
+        return (rule_tokens, addr_str)
+
+    def __init__(self, addr_rule_str):
+
+        self.addr_rule_str = addr_rule_str
+        self.rule_tokens, addr_str = AddressRule.extract_tokens(addr_rule_str)
+        Address.__init__(self, addr_str)
+
+    def __repr__(self):
+        return 'AddressRule(%r)' % self.addr_rule_str
+
+    def match(self, addr):
+
+        try:
+            cmp_val = cmp(self, addr)
+        except ValueError:
+            return False
+
+        if not self.rule_tokens:
+            return cmp_val == 0
+
+        print addr.number_pair, self.number_pair
+
+        for rule_token in self.rule_tokens:
+
+            if rule_token == u'單' and not addr.number_pair[0] & 1 == 1:
+                return False
+            if rule_token == u'雙' and not addr.number_pair[0] & 1 == 0:
+                return False
+            if u'以上' in rule_token and not addr.number_pair >= self.number_pair:
+                return False
+            if u'以下' in rule_token and not addr.number_pair <= self.number_pair:
+                return False
+            if rule_token == u'至' and not self.number_pair <= addr.number_pair <= Address.get_number_pair(self.tokens[self.first_number_token_idx+1]):
+                return False
+
+        return True
+
+test_cases = [
+    u'全', u'單全',
+    u'雙48號以下', u'雙50號以上',
+    u'7號',
+    u'連2之4號以上',
+    u'14號含附號', u'14巷全',
+    u'47號', u'47附號全',
+    u'雙68巷至70號含附號全',
+    u'連49號含附號以下',
+    u'1之3號及以上附號',
+]
+
+for test_case in test_cases:
+    addr_rule_str = u'臺北市信義區市府路'+test_case
+    addr_rule = AddressRule(addr_rule_str)
+    print addr_rule
+    print addr_rule.tokens
+    print addr_rule.rule_tokens
+    print
+
+addr_rule = AddressRule('臺北市信義區市府路14巷全')
+print addr_rule
+print addr_rule.tokens
+print addr_rule.rule_tokens
+print addr_rule.match(Address('臺北市信義區市府路14巷3號'))
+
+import sys; sys.exit()
 
 #def match(addr_tokens, rule_tokens):
 #
