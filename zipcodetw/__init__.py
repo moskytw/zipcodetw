b07e4eb5733c72811b1a2a4c1be8e2eda6ea624e
Mosky <mosky.tw@gmail.com>
2014-02-03 20:19:24 +0800

    added version

--- zipcodetw/__init__.py
+++ zipcodetw/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = '0.1'
+
 from os.path import join, normpath
 from zipcodetw.util import Directory
 
 _package_dir = normpath(join(__file__, '..'))
 
 _dir = Directory()
-_dir.load_chp_csv(open(join(_package_dir, 'zipcodetw-201311.csv')))
+_dir.load_chp_csv(open(join(_package_dir, 'data', 'zipcodetw-201311.csv')))
 
 find_zipcodes = _dir.find_zipcodes
 find = _dir.find
