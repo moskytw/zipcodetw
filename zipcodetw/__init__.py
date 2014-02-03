4fb8d1b0dd61ae763fcf85d661cc6440f9a845c1
Mosky <mosky.tw@gmail.com>
2014-02-03 21:56:01 +0800

    renamed var

--- zipcodetw/__init__.py
+++ zipcodetw/__init__.py
@@ -3,13 +3,13 @@
 
 __version__ = '0.1'
 
-from os.path import join, normpath
+from os.path import dirname, join
 from _zipcodetw import Directory
 
-_package_dir = normpath(join(__file__, '..'))
+_ROOT = dirname(__file__)
 
 _dir = Directory()
-_dir.load_chp_csv(open(join(_package_dir, '201311.csv')))
+_dir.load_chp_csv(open(join(_ROOT, '201311.csv')))
 
 find_zipcodes = _dir.find_zipcodes
 find = _dir.find
