360612a811d3104ceda6eb792ee0208f8d39f3ea
Mosky <mosky.tw@gmail.com>
2014-02-03 20:25:06 +0800

    added package files

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import zipcodetw
+
+setup(
+
+    name = 'zipcodetw',
+    version = zipcodetw.__version__,
+    description = 'Find Taiwan ZIP code by address quickly.',
+    long_description = open('README.rst').read(),
+
+    author = 'Mosky',
+    url = 'https://github.com/moskytw/zipcodetw',
+    author_email = 'mosky.tw@gmail.com',
+    license = 'MIT',
+    platforms = 'any',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'wan ZIP code by address quickly.ntended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    packages = find_packages(),
+
+)
+
