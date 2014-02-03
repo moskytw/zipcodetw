1db68f3500870b4031546d3a90d9d841ed603d5c
Mosky <mosky.tw@gmail.com>
2014-02-03 14:57:05 +0800

    added shortcuts

---
+++ zipcodetw/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os.path import join, normpath
+from zipcodetw.util import Directory
+
+_package_dir = normpath(join(__file__, '..'))
+
+_dir = Directory()
+_dir.load_chp_csv(open(join(_package_dir, 'zipcodetw-201311.csv')))
+
+find = _dir.find
+fuzzy_find = _dir.fuzzy_find
