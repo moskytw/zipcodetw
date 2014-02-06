moskytw/zipcodetw

commit 8c2e23de989f13f13c7c13af7352790b17921287

It seems slightly faster.

_zipcodetw.py
@@ -170,9 +170,11 @@ class Rule(Address):
 
             # the addr's tokens whose unit bigger than rule's are ignorable
             start_unit = my_tokens_to_match[0][Address.UNIT]
-            for his_start_pos, his_token in enumerate(addr.tokens):
-                if his_token[Address.UNIT] == start_unit:
-                    break
+            his_start_pos = 0
+            for his_token in addr.tokens:
+                if his_token[Address.UNIT] != start_unit:
+                    his_start_pos += 1
+                break
 
             his_tokens_to_match = addr.tokens[his_start_pos:his_end_pos]
             if len(my_tokens_to_match) != len(his_tokens_to_match):

commit ba48931b3530e5bb8193684af22a23fc60f2323d

fixed the bug caused by 8c2e23de

_zipcodetw.py
@@ -174,7 +174,8 @@ class Rule(Address):
             for his_token in addr.tokens:
                 if his_token[Address.UNIT] != start_unit:
                     his_start_pos += 1
-                break
+                else:
+                    break
 
             his_tokens_to_match = addr.tokens[his_start_pos:his_end_pos]
             if len(my_tokens_to_match) != len(his_tokens_to_match):