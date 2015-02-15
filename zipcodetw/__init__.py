72f798073e7a8814eba19af22d5fa5953f3a35ec
Mosky <mosky.tw@gmail.com>
2015-02-15 18:04:35 +0800

    updated the data to 201412

--- zipcodetw/__init__.py
+++ zipcodetw/__init__.py
@@ -8,8 +8,8 @@ from .util import Directory
 
 # paths
 _package_root = dirname(__file__)
-_chp_csv_path = join(_package_root, '201311.csv')
-_db_path = join(_package_root, '201311.db')
+_chp_csv_path = join(_package_root, '201412.csv')
+_db_path = join(_package_root, '201412.db')
 
 # make a directory
 _dir = Directory(_db_path)
