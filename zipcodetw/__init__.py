c7877ec5943e9ae48d13c8be32d5d6417dc9fa08
Mosky <mosky.tw@gmail.com>
2019-11-30 17:48:37 +0800

    Update the csv

--- zipcodetw/__init__.py
+++ zipcodetw/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '0.6.1'
+__version__ = '0.6.2'
 
 from os.path import dirname, join
 from .util import Directory
 
 # paths
 _package_root = dirname(__file__)
-_chp_csv_path = join(_package_root, '201412.csv')
-_db_path = join(_package_root, '201412.db')
+_chp_csv_path = join(_package_root, '20190415.csv')
+_db_path = join(_package_root, '20190415.db')
 
 # make a directory
 _dir = Directory(_db_path)
